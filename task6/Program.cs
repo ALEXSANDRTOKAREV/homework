5baf3e36de200b0e8827590698733f2dc91fc4a1
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-08-25 18:58:00 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a ;
+Console.WriteLine("Введите число:");
+a = Convert.ToInt32(Console.ReadLine());
+    
+if (a % 2 == 0)
+ 
+ {
+ Console.WriteLine("число чётное (делится  на два без остатка).");
+}
+
+else
+{
+
+ Console.WriteLine( "число нечётное ( не делится на два без остатка).");
+
+}
