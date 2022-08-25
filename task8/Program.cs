04436424650ac37aa674e3ae1f334446c21d7808
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-08-25 19:00:12 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;            
+while (true)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i+ " " , N);
+	}
+	if (N<=i )
+	{
+		break;
+	}
+	i++;
+}
