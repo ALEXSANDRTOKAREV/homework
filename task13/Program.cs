cf4116a629d7f17f3cf1660e137ff1c40eddf537
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-08-29 14:08:22 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string aText = Convert.ToString(a);
+if (aText.Length > 2)
+{
+  Console.WriteLine("третья цифра в этом числе :  " + aText[2]);
+}
+else
+{
+  Console.WriteLine("в это числе третьей цифры нет!");
+}
