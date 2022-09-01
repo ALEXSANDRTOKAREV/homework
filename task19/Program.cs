c6a35c06c79731525aef1d132a53417ae097e42a
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-01 18:58:45 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? a = Console.ReadLine();
+void NumberA(string a)
+{
+ if (a[0]==a[4] || a[1]==a[3])
+ {
+ Console.WriteLine("Палиндром");
+  }
+ else Console.WriteLine("Не палиндром");
+}
+if (a!.Length == 5)
+{
+NumberA(a);
+}
+else 
+Console.WriteLine("Вы ввели неправильное число");
