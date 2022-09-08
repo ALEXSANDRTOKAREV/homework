67ac0d54cc029325ae9a092f0b47d1cbe7168453
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-08 18:54:46 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int [10];
+int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write(array[i] + " ");
+
+        if (i % 2 != 0)
+        {
+            sum += array[i];  
+        }
+    }
+Console.WriteLine($" Сумма элементов, стоящих на нечётных позициях:{sum}");
