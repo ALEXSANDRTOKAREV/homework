c8ecee9db118f55466c3ff85c70345c5678b9e6e
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-12 19:39:24 +0300

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,20 @@
+﻿int N=123;
+int[] array=new int[N];
+Random random=new Random();
+
+for(int i=0;i<array.Length;i++)
+{
+array[i]=random.Next(0,123);}
+
+for(int i=0;i<array.Length;i++)
+{
+System.Console.Write($"{array[i],4}");
+}
+int count=0;
+for(var i=0; i<array.Length;i++)
+{
+if(i>10 && i<99)
+count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count );
