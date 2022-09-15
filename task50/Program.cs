747e8fee4cf85338effb44fbe9e65fa46310ac53
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-15 18:36:42 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.Write("Введите m(количество строк массива): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n(количество столбцов массива): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+int [,] array = new int [m , n];
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.Next(0,10) ;  
+         Console.Write("{0,9:10}", array[i, j]); 
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите координаты позиции элемента, разделенные пробелом: ");
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= m 
+&& position[1] <= n 
+&& position[0] >= 0 
+&& position[1] >= 0) 
+{
+  double result = array[position[0]-1, position[1]-1];
+  Console.Write($"Значение элемента: {result}");
+}
+else Console.Write($"такого элемента в массиве нет.");
+
+int[] ParserString(string input)
+{
+  int countNumbers = 1;
+  for (int i = 0; i < input.Length; i++)
+  {
+      if (input[i] == ',')
+          countNumbers++;
+  }
+
+  int[] numbers = new int[countNumbers];
+
+  int numberIndex = 0;
+  for(int i = 0; i < input.Length; i++)
+  {
+    string subString = String.Empty;
+
+    while (input[i] != ',')
+    {
+      subString += input[i].ToString();
+      if (i >= input.Length - 1)
+        break;
+      i++;
+    }
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+  }
+
+  return numbers;
+}
+string RemovingSpaces (string input)
+{
+  string output = String.Empty;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] != ' ') 
+    {
+      output += input[i];
+    }
+  }
+  return output;
+}
