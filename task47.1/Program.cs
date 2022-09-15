2bc775609bca93cf06037e280f621eb057441554
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-15 18:33:00 +0300

    task47

---
+++ task47.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] a = new double [m , n];
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        a[i, j] = random.NextDouble() * 100; 
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
