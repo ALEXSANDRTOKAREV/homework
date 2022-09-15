aef043c7713dab2768076ce5820c2dc62bb49789
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-15 18:38:48 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int [,] array = new int [m,n];
+int []sum = new int [n];
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.Next(0,100) ; 
+        Console.Write( array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        sum[i] +=array[j,i];
+
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    Console.Write((sum[i]/m) +  " ");
+}
+Console.ReadLine();
