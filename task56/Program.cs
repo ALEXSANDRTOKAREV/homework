f01f5fe226bf02c0f37503918bdab03b9d84a357
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-09-19 19:02:26 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int[,] array = new int[m, n];
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.Next(0, 100);
+        Console.Write(array[i, j] + " \t ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("строка с наименьшей суммой елементов под номером: " + (indexLine) + ", с суммой елементов равной: " + (minsum));
